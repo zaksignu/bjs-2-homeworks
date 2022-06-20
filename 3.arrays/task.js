5dda02ea9a64a2729e7e055b1fa8df8f86391249
zaksignu <zaksignu@gmail.com>
2022-06-20 14:38:31 +0300

    Final variant

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
 function compareArrays(arr1, arr2) {
   let result;
+  if (arr1.length == arr2.length) {
+    result = arr1.every((number,index)=> number === arr2[index]);
+  } else {
+    result = false;
+  }
+  return result;
+}
 
-  // Ваш код
-
-  return result; // boolean
+function name(params) {
+  
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  resultArr = arr.filter(number => number > 0).filter(number => number % 3 ===0).map(number => number*10);
+  return resultArr; 
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name=name;
+  this.gender=gender;
+  this.age=age;
+}
 
+function marksTouch(objct){
+  if(objct.marks === undefined){ 
+    objct.marks=[];
+  }
+    return 1
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject=subjectName
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  marksTouch(this);
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...mark) {
+  marksTouch(this);
+  this.marks.push(...mark)
+}
+
+Student.prototype.getAverage = function (){
+  return this.marks.reduce((acc,item,index,arr)=>{
+    acc +=item;
+    if (index === arr.length - 1) {
+      return  acc/arr.length;
+    }
+    return acc;
+  },0);
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
