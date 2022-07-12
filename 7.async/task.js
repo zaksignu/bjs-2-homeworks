4874e8f427bf2c5e7221209704f7c072e8d0f800
zaksignu <zaksignu@gmail.com>
2022-07-12 11:51:32 +0300

    FInal variant

---
+++ 7.async/task.js
@@ -0,0 +1,93 @@
+class AlarmClock{
+    constructor(){
+        this.alarmCollection = [];
+        this.id = 0;  
+        this.timerId = null;
+        this.tickPeriod = 10000;
+    }
+
+    addClock(date, funct, id) {
+        if (id == null){
+            throw new Error('ID не передан');
+        }
+        if (!this.alarmCollection.some((item)=>item[2] === id)) {
+            this.alarmCollection.push([date, funct,id]);
+        } else {
+            console.error("ID уже существует!");
+        }
+    }
+
+    getCurrentFormattedTime(){
+        let dt = new Date();
+        return dt.getHours()+":"+dt.getMinutes();
+    }
+
+    removeClock(id){
+
+        this.alarmCollection = this.alarmCollection.filter((item) => item[2] !==id).slice();
+        return this.alarmCollection.some((item) => item[2] !==id);
+    }
+
+    start(){
+          if (this.timerId === null){
+            this.timerId = setInterval(checkclock, this.tickPeriod,this);
+        }
+    }
+
+    stop(){
+        if (this.timerId !==null){
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    getCurrentFormattedTimePlus(numb){
+        let dt = new Date();
+        let newMin = dt.getMinutes()+numb;
+        return dt.getHours()+":"+newMin;
+    }
+
+    clearAlarms(){
+        if (this.timerId !== null){
+        clearInterval(this.timerId);
+        }
+        this.alarmCollection = [];
+        console.log("Alarms cleared")
+    }
+
+    printAlarms(){
+        console.log("Active alarms");
+        console.log("-------------");
+        this.alarmCollection.forEach((item)=> console.log(item[0]+" "+item[2]))
+        console.log("_____________");
+    }
+
+}
+
+function checkclock(ob){
+       let formattedDate = ob.getCurrentFormattedTime()
+       let alarms = ob.alarmCollection.filter((item) => item[0] === formattedDate);
+       alarms.forEach((item) => item[1](ob));
+}
+
+let tick = new AlarmClock;
+let firstTime = tick.getCurrentFormattedTime(0);
+let secondTime = tick.getCurrentFormattedTimePlus(1);
+let thirdTime = tick.getCurrentFormattedTimePlus(2);
+
+tick.addClock(firstTime,function() {
+    console.log("timer_callback_1");},1);
+
+tick.addClock(secondTime,function(ob) {
+        console.log("timer_callback_2");
+        ob.removeClock(2);   
+    },2);
+
+tick.addClock(thirdTime,function(ob) {
+            console.log("timer_callback_3");
+            clearInterval(ob.timerId);
+            ob.clearAlarms();
+            ob.printAlarms();
+        },3);
+tick.printAlarms();
+tick.start();
